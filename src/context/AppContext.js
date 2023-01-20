504b0bdbea9ddfffa996178e6f8e65594aa10594
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-01-20 18:43:56 +0100

    Added addExpense to AppContext

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,9 +2,11 @@ import { createContext, useReducer, useEffect  } from "react";
 import React from "react";
 import axios from "axios";
 
+
 const AppReducer = (state, action) => {
   switch (action.type) {
     case "ADD_EXPENSE":
+      addExpense(action.payload);
       return {
         ...state,
         expenses: [action.payload, ...state.expenses],
@@ -40,6 +42,43 @@ const updateAmount = (newAmount) => {
     })
     .catch((err) => console.log(err));
 };
+
+// const addExpense = (newExpense) => {
+//   var newState;
+//   axios
+//       .get("http://localhost:8000/budget")
+//       .then((res) => {
+//         // Store the data in the initial state
+//        newState = { ...initialState.expenses, newExpense };
+//        console.log('newState', newState)
+
+//       })
+//       .catch((err) => console.log(err));
+
+//    axios
+//     .patch("http://localhost:8000/budget", { expenses: newState })
+//     .then((res) => {
+//       console.log('res', res.data)
+//     })
+//     .catch((err) => console.log(err));
+    
+// }
+const addExpense = (newExpense) => {
+  axios
+    .get("http://localhost:8000/budget")
+    .then((res) => {
+      let newExpenses = [...res.data.expenses, newExpense];
+      axios
+        .patch("http://localhost:8000/budget", { expenses: newExpenses })
+        .then((res) => {
+          console.log("Expense added successfully");
+          // dispatch({ type: "SET_INITIAL_STATE", payload: { ...state, expenses: newExpenses } });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+};
+
 
 const initialState = {
   budget: 0,
