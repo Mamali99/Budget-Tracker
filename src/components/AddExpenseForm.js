31cd20d3598dea9934dd2c16d547fac00f9232b4
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-01-21 12:11:06 +0100

    Changed the position of the Button in AddExpenseForm

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,35 +1,31 @@
 import React, { useState, useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import {v4 as uuidv4} from 'uuid';
-
-
+import { v4 as uuidv4 } from "uuid";
 
 export default function AddExpenseForm() {
   const [name, setName] = useState("");
   const [cost, setCost] = useState("");
   const [date, setDate] = useState("");
-  const {dispatch} = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const expense = {
-        id: uuidv4(),
-        name: name,
-        cost: parseInt(cost),
-        date: date
-    }
-    console.log('expense', expense)
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+      date: date,
+    };
+    console.log("expense", expense);
     dispatch({
-        type: 'ADD_EXPENSE',
-        payload: expense
-    })
-    setName('');
-    setCost('');
-    setDate('');
-      
-  }
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+    setName("");
+    setCost("");
+    setDate("");
+  };
   return (
-    
     <form onSubmit={handleSubmit}>
       <div className="row">
         <div className="col-sm">
@@ -40,7 +36,7 @@ export default function AddExpenseForm() {
             className="form-control"
             id="name"
             value={name}
-            onChange={(e)=>setName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="col-sm">
@@ -51,27 +47,28 @@ export default function AddExpenseForm() {
             className="form-control"
             id="cost"
             value={cost}
-            onChange={(e)=>setCost(e.target.value)}
+            onChange={(e) => setCost(e.target.value)}
           />
         </div>
         <div className="col-sm">
-      <label for="date">Date</label>
-      <input
-        type="date"
-        required="required"
-        className="form-control"
-        id="date"
-        value={date}
-        onChange={(e)=>setDate(e.target.value)}
-      />
-    </div>
-        <div className="col-sm mt-3 text-center">
-          <button type="submit" className="btn btn-primary">
-            Save
-          </button>
+          <label for="date">Date</label>
+          <input
+            type="date"
+            required="required"
+            className="form-control"
+            id="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div className="col-sm mt-2">
+          <div className="mt-3">
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </div>
         </div>
       </div>
     </form>
-   
   );
 }
